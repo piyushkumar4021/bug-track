1c34149acd39641e849d8141159a75eb4153574c
unknown <piyushkumar4075@gmail.com>
2025-04-18 00:53:03 +0530

    build api for deleting a bug

--- src/app/api/bugs/[id]/route.ts
+++ src/app/api/bugs/[id]/route.ts
@@ -21,9 +21,8 @@ export async function GET(
 
 export async function PATCH(
   req: NextRequest,
-  { params }: { params: { id: string } }
+  { params: { id } }: { params: { id: string } }
 ) {
-  const { id } = await params;
   const validatedId = parseInt(id);
 
   if (!validatedId)
@@ -56,4 +55,27 @@ export async function PATCH(
   });
 
   return NextResponse.json(updatedBug, { status: 200 });
+}
+
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const validatedId = parseInt(params.id);
+  if (!validatedId)
+    return NextResponse.json({ error: 'Invalid Id' }, { status: 400 });
+
+  try {
+    const bug = await prisma.bug.findUnique({ where: { id: validatedId } });
+    if (!bug)
+      return NextResponse.json({ error: 'bug not found' }, { status: 404 });
+
+    await prisma.bug.delete({ where: { id: validatedId } });
+    return NextResponse.json({});
+  } catch {
+    return NextResponse.json(
+      { error: 'Internal server error' },
+      { status: 500 }
+    );
+  }
 }
